ec8aee0d25bc653182b5457589807fd8f2a49e11
Dani97j <dani.macas97@hotmail.com>
2020-08-17 00:41:03 -0500

    ver productos

--- public/js/cargarProductos.js
+++ public/js/cargarProductos.js
@@ -1,5 +1,5 @@
 var referenciaProductos = db.ref("productos");
-
+/*
   function mostrarProductoFirebase(){
       referenciaProductos.on("value", function(snapshot){
           var datos = snapshot.val();
@@ -15,4 +15,30 @@ var referenciaProductos = db.ref("productos");
       
 
       })
+  }*/
+  function innerHTML(id, result){
+      return document.getElementById(id).innerHTML+=result;
+  
+  }
+  function datos(nombre,precio,imagen){
+    return`<div class="thumbnail thumbnail-content-phones">
+            <img src="${imagen}" alt="prod-icon" class="img-responsive" width=20>
+             <div class="caption">
+                <h3 class=" text-center">${nombre}</h3>										        
+                <p class="text-justify">
+                   $ ${precio}
+                </p>
+                <input id="cantidad" type="number" class="form-control" required />
+                <p class="text-center"><a href="#" class="btn btn-danger" role="button">Agregar</a></p>
+            </div>
+            </div> `;
+  }
+  function verProducto(){
+      var task  = db.ref("productos/");
+      task.on("child_added",function(data){
+          var taskValue = data.val();
+          var productos = datos(taskValue.nombre,taskValue.precio,taskValue.imagen);
+          innerHTML("imagen1",productos);
+
+      });
   }

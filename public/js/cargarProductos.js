1d0a71623011fd440500e2f5a29589f21548b479
Dani97j <dani.macas97@hotmail.com>
2020-08-17 01:14:25 -0500

    productos de bien

--- public/js/cargarProductos.js
+++ public/js/cargarProductos.js
@@ -21,17 +21,18 @@ var referenciaProductos = db.ref("productos");
   
   }
   function datos(nombre,precio,imagen){
-    return`<div class="thumbnail thumbnail-content-phones">
-            <img src="${imagen}" alt="prod-icon" class="img-responsive" width=20>
+    return`<div class="col-xs-12 col-sm-6 col-md-3">
+          <div class="thumbnail thumbnail-content-phones">
+            <img src="${imagen}" alt="prod-icon" class="img-responsive">
              <div class="caption">
-                <h3 class=" text-center">${nombre}</h3>										        
-                <p class="text-justify">
+                <h3 class=" text-center">${nombre}</h3>	                								        
+                <class="text-left">
                    $ ${precio}
-                </p>
-                <input id="cantidad" type="number" class="form-control" required />
-                <p class="text-center"><a href="#" class="btn btn-danger" role="button">Agregar</a></p>
+                <input id="cantidad" type="number"  min="1" max="100" value="1" required />
+                <a href="#" class="btn btn-danger" role="button" align="right">Agregar</a>             
             </div>
-            </div> `;
+          </div>
+          </div> `;
   }
   function verProducto(){
       var task  = db.ref("productos/");

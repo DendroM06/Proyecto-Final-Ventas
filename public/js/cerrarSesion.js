46d39b35aa186bd9933f339f0cace0fef67f462f
DendroM06 <dmunoz5628@gmail.com>
2020-08-16 21:49:42 -0500

    version 7

--- public/js/cerrarSesion.js
+++ public/js/cerrarSesion.js
@@ -1,9 +1,9 @@
-  //Boton cerrar Sesion
-  const logout = document.querySelector('#cerrarSesion');
-logout.addEventListener('click', e => {
-    e.preventDefault();
-    auth.signOut().then(() => {
-        console.log('A salido del Sistema')
-        location.href = "../login_registrarse.html";
-    })
-})
+//Boton cerrar Sesion
+const logout = document.querySelector("#cerrarSesion");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("A salido del Sistema");
+    location.href = "../login_registrarse.html";
+  });
+});

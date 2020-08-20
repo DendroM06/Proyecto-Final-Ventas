01ea83bd19dea8c677fd67336f1952734c0cf9e1
Dani97j <dani.macas97@hotmail.com>
2020-08-20 06:39:02 -0500

    ya esta

--- public/js/productoPrincipal.js
+++ public/js/productoPrincipal.js
@@ -11,11 +11,11 @@ var referenciaProductos = db.ref("productos");
             <img src="${imagen}" alt="prod-icon" class="img-responsive" width="225" height="225">
              <div class="caption">
                 <h3 class=" text-center">${nombre}</h3>	                								        
-                <h1 class="card-title pricing-card-title text-center precio" text-align="center">
+                <h4 class="card-title pricing-card-title text-center precio" text-align="center">
                    $ 
                    <span class="">${precio}
                    </span>
-                </h1> 
+                </h4> 
                 <p style="text-align: center;">
                 <input id="cantidad" type="number"  min="1" max="100" value="1" required />
                 </p> 
@@ -38,31 +38,5 @@ var referenciaProductos = db.ref("productos");
       });
      
   }
-  function datosWh(nombre,precio,imagen,categoria){
-    if(categoria == 'Cerveza')
-      return`<div class="col-xs-12 col-sm-6 col-md-3" >
-          <div class="thumbnail thumbnail-content-phones">
-            <img src="${imagen}" alt="prod-icon" class="img-responsive">
-             <div class="caption">
-                <h3 class=" text-center">${nombre}</h3>	                								        
-                <h4 class="card-title pricing-card-title text-center precio" text-align="center">
-                   $ 
-                   <span class="">${precio}
-                   </span>
-                </h4>                        
-            </div>
-          </div>
-          </div> `   
-  }
-  function verProductoC(){
-    var task  = db.ref("productos/");
-    task.on("child_added",function(data){
-        var taskValue = data.val();        
-        var productos = datosWh(taskValue.nombre,taskValue.precio,taskValue.imagen,task.categoria);
-        innerHTML("imagen1",productos);
-       //console.log(productos);
-
-    });
-}
-
+ 
 

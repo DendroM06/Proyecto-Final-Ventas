6c8b788aed13471fb575cbe49c39fabe7ed44265
RonaldGuerrero <guerreroronaldo@hotmail.com>
2020-08-20 00:14:03 -0500

    GEFinal

--- public/js/gestionEntrega.js
+++ public/js/gestionEntrega.js
@@ -1,4 +1,4 @@
-var referenciaPUsuarios = db.ref("usuarios/");
+/*var referenciaPUsuarios = db.ref("usuarios/");
 
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
@@ -6,36 +6,29 @@ firebase.auth().onAuthStateChanged(function (user) {
     referenciaPUsuarios.on("value", function (datos) {
       var dato = datos.val();
       $.each(dato, function (node, value) {
-        if (value.uid == uid) {
-          var id_cliente = node;
-          console.log(id_cliente);
-          var reference = db.ref("pedidosRG/");
-          reference.on("value", function (datos) {
-            var data = datos.val();
-            $.each(data, function (id_pedido, value) {
-              if (value.id_cliente == id_cliente) {
-                var sendData = table(
-                  id_pedido,
-                  value.total,
-                  value.fecha_pedido,
-                  value.fecha_entrega,
-                  value.estado
-                );
-                printHTML("loadTable", sendData);
-                var reference2 = db.ref("clientesRG/" + id_cliente);
-                reference2.on("value", function (datos2) {
-                  var data2 = datos2.val();
-                  document.getElementById("nombreCliente").innerText =
-                    data2.nombres + " " + data2.apellidos;
-                });
-              }
-            });
-          });
-        }
+        if (value.uid == uid) {*/
+var id_cliente = '-MEzpCbszm-DlmSJ6JgW'//node;
+console.log(id_cliente);
+var reference = db.ref("pedidosRG/");
+reference.on("value", function (datos) {
+  var data = datos.val();
+  $.each(data, function (id_pedido, value) {
+    if (value.id_cliente == id_cliente) {
+      var sendData = table(id_pedido, value.total, value.fecha_pedido, value.fecha_entrega, value.estado);
+      printHTML("loadTable", sendData);
+      var reference2 = db.ref("usuarios/" + id_cliente);
+      reference2.on("value", function (datos2) {
+        var data2 = datos2.val();
+        document.getElementById("nombreCliente").innerText = data2.nombre + " " + data2.apellido;
       });
-    });
-  }
+    }
+  });
 });
+/*}
+});
+});
+}
+});*/
 
 function printHTML(request, response) {
   return (document.getElementById(request).innerHTML += response);
@@ -43,27 +36,11 @@ function printHTML(request, response) {
 
 function table(id_pedido, total, fecha_pedido, fecha_entrega, estado) {
   if (estado == "Pendiente") {
-    return (
-      "<tr><td>" +
-      id_pedido +
-      '<td><a href="#" data-toggle="modal" data-target="#modalTask" onclick="viewDataPedido(\'' +
-      id_pedido +
-      "','" +
-      estado +
-      "')\">" +
+    return ("<tr><td>" + id_pedido +
+      '<td><a href="#" data-toggle="modal" data-target="#modalTask" onclick="viewDataPedido(\'' + id_pedido + "','" + estado + "')\">" +
       '<i class="fas fa-clipboard-list text-success icon-lg"></i></a></td>' +
-      "</td><td>" +
-      total +
-      "</td><td>" +
-      fecha_pedido +
-      "</td><td>" +
-      fecha_entrega +
-      "</td><td>" +
-      estado +
-      "</td>" +
-      '<td><a href="#" data-toggle="modal" data-target="#modalDelete" onclick="idEliminarPedido(\'' +
-      id_pedido +
-      "')\">" +
+      "</td><td>" + total + "</td><td>" + fecha_pedido + "</td><td>" + fecha_entrega + "</td><td>" + estado + "</td>" +
+      '<td><a href="#" data-toggle="modal" data-target="#modalDelete" onclick="idEliminarPedido(\'' + id_pedido + "')\">" +
       '<i class="fas fa-trash-alt red icon-lg"></i></a></td></tr>'
     );
   } else {
@@ -98,27 +75,17 @@ function viewDataPedido(id_pedido, estado) {
     var _subtotal = 0;
     $.each(data, function (id_detallePedido, value) {
       if (value.id_pedido == id_pedido) {
-        var reference2 = db.ref("productosRG/" + value.id_producto);
+        var reference2 = db.ref("productos/" + value.id_producto);
+        var _subt;
         reference2.on("value", function (datos2) {
           var data2 = datos2.val();
-          _subtotal += value.subtotal;
-          document.getElementById(
-            "labelSubtotal"
-          ).innerText = _subtotal.toFixed(2);
-          document.getElementById("labelIva").innerText = (
-            _subtotal * 0.12
-          ).toFixed(2);
-          document.getElementById("labelTotal").innerText = (
-            _subtotal +
-            _subtotal * 0.12
-          ).toFixed(2);
-          var sendData = table2(
-            data2.producto,
-            data2.costo,
-            value.cantidad,
-            value.subtotal
-          );
-          printHTML("loadTable2", sendData);
+          _subt = parseFloat(data2.precio) * parseFloat(value.cantidad);
+          var sendData = table2(data2.categoria,data2.nombre, data2.precio, value.cantidad, _subt);
+          printHTML("loadTable2", sendData);         
+          _subtotal += _subt;
+          document.getElementById("labelSubtotal").innerText = _subtotal.toFixed(2);
+          document.getElementById("labelIva").innerText = (_subtotal * 0.12).toFixed(2);
+          document.getElementById("labelTotal").innerText = (_subtotal + _subtotal * 0.12).toFixed(2);
         });
       }
     });
@@ -154,9 +121,11 @@ function display(request, response) {
   return (document.getElementById(request).style.display = response);
 }
 
-function table2(producto, costo, cantidad, subtotal) {
+function table2(categoria,producto, costo, cantidad, subtotal) {
   return (
     "<tr><td>" +
+    categoria +
+    "</td><td>" +
     producto +
     "</td><td>" +
     costo +

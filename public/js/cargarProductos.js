a4df1cac49cbfa793274b1b8ad066eedf2dcb3f1
Dani97j <dani.macas97@hotmail.com>
2020-08-20 04:51:53 -0500

    cervezabienhecho

--- public/js/cargarProductos.js
+++ public/js/cargarProductos.js
@@ -3,6 +3,7 @@ var referenciaProductos = db.ref("productos");
 function vertodosProductos(){
   verProducto();
   verProductoW();
+  verProductoC();
 }
 
 function innerHTML(id, result) {
@@ -81,3 +82,46 @@ function verProductoW(){
      //console.log(productos);
   });
 }
+
+function datosC(nombre, precio, imagen, id, categoria) {
+  if (categoria == 'Cerveza'){
+    return `<div class="col-xs-12 col-sm-6 col-md-3" >
+          <div class="thumbnail thumbnail-content-phones">
+            <img src="${imagen}" alt="prod-icon" class="img-responsive">
+             <div class="caption">
+                <h3 class=" text-center">${nombre}</h3>	                								        
+                <h1 class="card-title pricing-card-title text-center precio" text-align="center">
+                   $ 
+                   <span class="">${precio}
+                   </span>
+                </h1>   
+                <input id="cantidad" type="number"  min="1" max="100" value="1" required />
+                <a class="btn btn-primary agregar-carrito" data-id="${id}">Agregar</a>             
+            </div>
+          </div>
+          </div> `;
+
+  }else if(categoria == 'Cerveza'){
+    return`<a><a>`;
+       
+  }
+  else {
+    return`<a><a>`;
+         
+  }
+
+    
+}
+
+function verProductoC(){
+  var task  = db.ref("productos/");
+  task.on("child_added",function(data){
+      var taskValue = data.val();
+      taskValue.id = data.key;
+      
+      var productos = datosC(taskValue.nombre,taskValue.precio,taskValue.imagen,taskValue.id,taskValue.categoria);
+      innerHTML("tcerveza",productos);
+     //console.log(productos);
+  });
+}
+

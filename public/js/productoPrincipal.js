33c03afe7a7cd473949d3292bccf1be199477e24
Dani97j <dani.macas97@hotmail.com>
2020-08-20 05:40:15 -0500

    inicio

--- public/js/productoPrincipal.js
+++ public/js/productoPrincipal.js
@@ -6,9 +6,9 @@ var referenciaProductos = db.ref("productos");
   }
   function datos(nombre,precio,imagen, id){
     //var i = i+1;
-    return`<div class="col-xs-12 col-sm-6 col-md-3" >
-          <div class="thumbnail thumbnail-content-phones">
-            <img src="${imagen}" alt="prod-icon" class="img-responsive">
+    return`<div class="col-xs-12 col-sm-6 col-md-3" style="width:240px heigth:70px">
+          <div class="thumbnail thumbnail-content-phones"style="width:240px heigth:70px">
+            <img src="${imagen}" alt="prod-icon" class="img-responsive" width="225" height="225">
              <div class="caption">
                 <h3 class=" text-center">${nombre}</h3>	                								        
                 <h4 class="card-title pricing-card-title text-center precio" text-align="center">
@@ -18,7 +18,7 @@ var referenciaProductos = db.ref("productos");
                 </h4>     
                 <input id="cantidad" type="number"  min="1" max="100" value="1" required />
                 <p>
-                <a class="btn btn-primary agregar-carrito" data-id="${id}">Agregar</a>  
+                <a class="btn btn-centre agregar-carrito" data-id="${id}">Agregar</a>  
                 </p>               
             </div>
           </div>

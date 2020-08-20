5c562616c13d4f04e7a4a320063ba7f0f8dab18e
DendroM06 <dmunoz5628@gmail.com>
2020-08-20 02:51:39 -0500

    Eleana

--- public/js/cargarProductos.js
+++ public/js/cargarProductos.js
@@ -1,41 +1,11 @@
 var referenciaProductos = db.ref("productos");
 
-  function innerHTML(id, result){
-      return document.getElementById(id).innerHTML+=result;
-  
-  }
-  function datos(nombre,precio,imagen, id){
-    //var i = i+1;
-    return`<div class="col-xs-12 col-sm-6 col-md-3" >
-          <div class="thumbnail thumbnail-content-phones">
-            <img src="${imagen}" alt="prod-icon" class="img-responsive">
-             <div class="caption">
-                <h3 class=" text-center">${nombre}</h3>	                								        
-                <h1 class="card-title pricing-card-title text-center precio" text-align="center">
-                   $ 
-                   <span class="">${precio}
-                   </span>
-                </h1>   
-                <input id="cantidad" type="number"  min="1" max="100" value="1" required />
-                <a class="btn btn-primary agregar-carrito" data-id="${id}" id="jol">Agregar</a>             
-            </div>
-          </div>
-          </div> `;
-  }
-  function verProducto(){
-      var task  = db.ref("productos/");
-      task.on("child_added",function(data){
-          var taskValue = data.val();
-          task.id = data.key;
-          var productos = datos(taskValue.nombre,taskValue.precio,taskValue.imagen,task.id);
-          innerHTML("imagen1",productos);
-         //console.log(productos);
-
-      });
-  }
-  function datosWh(nombre,precio,imagen, id, categoria){
-    if(categoria == 'Cerveza')
-      return`<div class="col-xs-12 col-sm-6 col-md-3" >
+function innerHTML(id, result) {
+  return (document.getElementById(id).innerHTML += result);
+}
+function datos(nombre, precio, imagen, id) {
+  //var i = i+1;
+  return `<div class="col-xs-12 col-sm-6 col-md-3" >
           <div class="thumbnail thumbnail-content-phones">
             <img src="${imagen}" alt="prod-icon" class="img-responsive">
              <div class="caption">
@@ -49,16 +19,41 @@ var referenciaProductos = db.ref("productos");
                 <a class="btn btn-primary agregar-carrito" data-id="${id}">Agregar</a>             
             </div>
           </div>
-          </div> `   
-  }
-  function verProductoC(){
-    var task  = db.ref("productos/");
-    task.on("child_added",function(data){
-        var taskValue = data.val();
-        task.id = data.key;
-        var productos = datosWh(taskValue.nombre,taskValue.precio,taskValue.imagen,task.id,task.categoria);
-        innerHTML("imagen1",productos);
-       //console.log(productos);
-
-    });
+          </div> `;
 }
+function verProducto() {
+  var task = db.ref("productos/");
+  task.on("child_added", function (data) {
+    var taskValue = data.val();
+    task.id = data.key;
+    var productos = datos(
+      taskValue.nombre,
+      taskValue.precio,
+      taskValue.imagen,
+      task.id
+    );
+    innerHTML("imagen1", productos);
+    //console.log(productos);
+  });
+}
+function datosWh(nombre, precio, imagen, id, categoria) {
+  if (categoria == "Cerveza")
+    return `<div class="col-xs-12 col-sm-6 col-md-3" >
+          <div class="thumbnail thumbnail-content-phones">
+            <img src="${imagen}" alt="prod-icon" class="img-responsive">
+             <div class="caption">
+                <h3 class=" text-center">${nombre}</h3>	                								        
+                <h1 class="card-title pricing-card-title text-center precio" text-align="center">
+                   $ 
+                   <span class="">${precio}
+                   </span>
+                </h1>   
+                <input id="cantidad" type="number"  min="1" max="100" value="1" required />
+                <a class="btn btn-primary agregar-carrito" data-id="${id}">Agregar</a>             
+            </div>
+          </div>
+          </div> `;
+}
+
+
+

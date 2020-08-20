a4276e85623141591d97f3c579f1ab3d71715d23
DendroM06 <dmunoz5628@gmail.com>
2020-08-20 03:53:19 -0500

    lolo

--- public/js/cargarProductos.js
+++ public/js/cargarProductos.js
@@ -56,15 +56,6 @@ function datosWh(nombre, precio, imagen, id, categoria) {
 }
 
 
-<<<<<<< HEAD
-=======
-        //return document.getElementById('cer').style.display='none';
-    
-   
-  }
-    
-    
-}
 function verProductoW(){
   var task  = db.ref("productos/");
   task.on("child_added",function(data){
@@ -74,7 +65,5 @@ function verProductoW(){
       var productos = datosW(taskValue.nombre,taskValue.precio,taskValue.imagen,taskValue.id,taskValue.categoria);
       innerHTML("twhisky",productos);
      //console.log(productos);
-    
-    
->>>>>>> bf8d0b9a5c110ead4d6276b33d6ebfca1996a3b5
-
+  });
+}

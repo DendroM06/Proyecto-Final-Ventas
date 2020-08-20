e376471c3c764b0b4bfb4149571accb6bec0875c
RonaldGuerrero <guerreroronaldo@hotmail.com>
2020-08-19 20:19:20 -0500

    GE005

--- public/js/gestionEntrega.js
+++ public/js/gestionEntrega.js
@@ -1,5 +1,6 @@
-var id_cliente;// = '-MEwmkudxZnaCJM2MLFu';
-///
+var id_cliente; /*= '-MEzpCbszm-DlmSJ6JgW'*/
+/// 
+
 var referenciaUsuarios = db.ref("usuarios/");
 firebase.auth().onAuthStateChanged(function (user) {  
   if (user) {
@@ -21,7 +22,7 @@ reference.on('value', function (datos) {
         }
     });
 });
-});
+
 
 function printHTML(request, response) {
     return document.getElementById(request).innerHTML += response;
@@ -66,6 +67,7 @@ function viewDataPedido(id_pedido, estado) {
             }
         });
     });
+    
     id_confirmarPedido = id_pedido;
     switch (estado) {
         case "Pendiente":
